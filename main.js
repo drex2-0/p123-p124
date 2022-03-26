3f6254baa65a410330894af8dd901147a14a3742
drex2-0 <84222894+drex2-0@users.noreply.github.com>
2022-03-26 13:39:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+nosex=0;
+nosey=0;
+difference=0;
+rightwristx=0;
+leftwristx=0;
+
 function setup()
 {
     video=createCapture(VIDEO);
@@ -17,9 +23,19 @@ function gotposes(results)
     if(results.length>0)
     {
         console.log(results);
+        nosex=results[0].pose.nose.x;
+        nosey=results[0].pose.nose.y;
+        rightwristx=results[0].pose.rightWrist.x;
+        leftwristx=results[0].pose.leftWrist.x;
+        difference=floor(leftwristx-rightwristx);
     }
 }
 function draw()
 {
     background('#aaa9ad');
+    fill("#4169e1");
+    stroke("#EFEBE0");
+    text("Padmaj",nosex,nosey);
+    textSize(difference);
+    document.getElementById("font_size").innerHTML="The font size of the text will be= "+difference+"px";
 }
